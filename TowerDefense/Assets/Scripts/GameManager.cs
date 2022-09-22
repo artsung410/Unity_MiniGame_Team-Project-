6390d592fcd85d3a82cf6b320e952b829198a5cc
usrrt <gkenfktm@gmail.com>
2022-09-22 18:06:32 +0900

    [Feat] 플레이어 수정 및 게임매니저 추가

--- TowerDefense/Assets/Scripts/GameManager.cs
+++ TowerDefense/Assets/Scripts/GameManager.cs
@@ -2,7 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : SingletoneBehaviour
 {
-    public bool IsDead;
+    #region 플레이어관련
+
+    public float PlayerSpeed;
+
+
+    #endregion
+
+
+
+    #region 포탑관련
+    public int TowerDamage;
+    public float TowerAttackArea;
+    public float TowerAttackSpeed;
+
+    public int TowerHP;
+    public int TowerLevel;
+
+    public int TowerPrice;
+
+    #endregion
+
+
+
+
+
+    #region 적관련
+    public float EnemySpeed;
+    public int EnemyHP;
+    public float EnemyAttackSpeed;
+
+    public int EnemyExp;
+    public int EnemyGold;
+    public float EnemyAttackArea;
+
+    #endregion
+
+
+
+
+    #region 웨이브
+    public int RespawnTime;
+    public int RespawnEnemyCount;
+
+    #endregion
 }
